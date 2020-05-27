a233d0676fe97bf1471fe42fb9fb77650fd2c5b5
KossBoii <long.truong0320@gmail.com>
2020-05-27 11:34:03 -0700

    add aug to config

--- samples/roadstress/roadstress.py
+++ samples/roadstress/roadstress.py
@@ -177,7 +177,7 @@ def train(model):
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=1,
-                layers='heads')
+                layers='heads', augmentation=True)
 
 
 def color_splash(image, mask):
